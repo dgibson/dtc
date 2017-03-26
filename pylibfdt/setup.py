14c4171f4f9ad2674249e06c54eb9ce0b533d4b6
Simon Glass <sjg@chromium.org>
2017-03-26 13:06:20 -0600

    pylibfdt: Use package_dir to set the package directory
    At present we manually move _libfdt.so into the correct place. Provide a
    package directory so we can avoid needing to do this.
    
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>

--- pylibfdt/setup.py
+++ pylibfdt/setup.py
@@ -5,6 +5,7 @@ setup.py file for SWIG libfdt
 
 Files to be built into the extension are provided in SOURCES
 C flags to use are provided in CPPFLAGS
+Object file directory is provided in OBJDIR
 """
 
 from distutils.core import setup, Extension
@@ -14,6 +15,7 @@ import sys
 progname = sys.argv[0]
 files = os.environ['SOURCES'].split()
 cflags = os.environ['CPPFLAGS'].split()
+objdir = os.environ['OBJDIR']
 
 libfdt_module = Extension(
     '_libfdt',
@@ -26,5 +28,6 @@ setup (name = 'libfdt',
        author      = "Simon Glass <sjg@chromium.org>",
        description = """Python binding for libfdt""",
        ext_modules = [libfdt_module],
+       package_dir = {'': objdir},
        py_modules = ["libfdt"],
        )
