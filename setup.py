23b56cb7e18992650c79a04c9e4e3f2740bc1fbd
Rob Herring <robh@kernel.org>
2021-11-10 19:11:35 -0600

    pylibfdt: Move setup.py to the top level
    Using 'pip' and several setup.py sub-commands currently don't work with
    pylibfdt. The primary reason is Python packaging has opinions on the
    directory structure of repositories and one of those appears to be the
    inability to reference source files outside of setup.py's subtree. This
    means a sdist cannot be created with all necessary source components
    (i.e. libfdt headers). Moving setup.py to the top-level solves these
    problems.
    
    With this change. the following commands now work:
    
    Creating packages for pypi.org:
    ./setup.py sdist bdist_wheel
    
    Using pip for installs:
    pip install .
    pip install git+http://github.com/robherring/dtc.git@pypi-v2
    
    Signed-off-by: Rob Herring <robh@kernel.org>
    Message-Id: <20211111011135.2386773-5-robh@kernel.org>
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>

--- setup.py
+++ setup.py
@@ -15,36 +15,37 @@ import os
 import re
 import sys
 
+srcdir = os.path.dirname(__file__)
+
 def get_top_builddir():
     if '--top-builddir' in sys.argv:
         index = sys.argv.index('--top-builddir')
         sys.argv.pop(index)
         return sys.argv.pop(index)
     else:
-        return os.getcwd()
+        return srcdir
 
-srcdir = os.path.dirname(os.path.abspath(sys.argv[0]))
 top_builddir = get_top_builddir()
 
 libfdt_module = Extension(
     '_libfdt',
-    sources=[os.path.join(srcdir, 'libfdt.i')],
-    include_dirs=[os.path.join(srcdir, '../libfdt')],
+    sources=[os.path.join(srcdir, 'pylibfdt/libfdt.i')],
+    include_dirs=[os.path.join(srcdir, 'libfdt')],
     libraries=['fdt'],
     library_dirs=[os.path.join(top_builddir, 'libfdt')],
-    swig_opts=['-I' + os.path.join(srcdir, '../libfdt')],
+    swig_opts=['-I' + os.path.join(srcdir, 'libfdt')],
 )
 
 setup(
     name='libfdt',
     use_scm_version={
-        "root": os.path.join(srcdir, '..'),
+        "root": srcdir,
     },
     setup_requires = ['setuptools_scm'],
     author='Simon Glass',
     author_email='sjg@chromium.org',
     description='Python binding for libfdt',
     ext_modules=[libfdt_module],
-    package_dir={'': srcdir},
+    package_dir={'': os.path.join(srcdir, 'pylibfdt')},
     py_modules=['libfdt'],
 )
