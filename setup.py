dgibson/dtc

commit 1cc41b1c969f1fa5090b166397e4bab4ab1aa449

pylibfdt: Add packaging metadata
PyPI expects to have various package metadata including long
description, license, and classifiers. Add them.

Signed-off-by: Rob Herring <robh@kernel.org>
Message-Id: <20211112041633.741598-3-robh@kernel.org>
Signed-off-by: David Gibson <david@gibson.dropbear.id.au>

setup.py
@@ -17,6 +17,9 @@ import sys
 
 srcdir = os.path.dirname(__file__)
 
+with open("README", "r") as fh:
+    long_description = fh.read()
+
 def get_top_builddir():
     if '--top-builddir' in sys.argv:
         index = sys.argv.index('--top-builddir')
@@ -49,4 +52,18 @@ setup(
     ext_modules=[libfdt_module],
     package_dir={'': os.path.join(srcdir, 'pylibfdt')},
     py_modules=['libfdt'],
+
+    long_description=long_description,
+    long_description_content_type="text/plain",
+    url="https://git.kernel.org/pub/scm/utils/dtc/dtc.git",
+    license="BSD",
+    license_files=["GPL", "BSD-2-Clause"],
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Operating System :: OS Independent",
+    ],
+
 )

MANIFEST.in
@@ -1,6 +1,9 @@
 # SPDX-License-Identifier: (GPL-2.0-or-later OR BSD-2-Clause)
 
 global-exclude *
+include README
+include GPL
+include BSD-2-Clause
 include setup.py
 include pylibfdt/libfdt.i
 include pylibfdt/*.py

commit 4048aed12b81c5a0154b9af438edc99ec7d2b6a1

setup.py: fix out of tree build
Fixes: commit 1cc41b1c9 ("pylibfdt: Add packaging metadata")

Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
Message-Id: <20220103073855.1468799-3-marcandre.lureau@redhat.com>
Signed-off-by: David Gibson <david@gibson.dropbear.id.au>

setup.py
@@ -17,7 +17,7 @@ import sys
 
 srcdir = os.path.dirname(__file__)
 
-with open("README", "r") as fh:
+with open(os.path.join(srcdir, "README"), "r") as fh:
     long_description = fh.read()
 
 def get_top_builddir():