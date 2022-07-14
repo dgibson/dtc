fda71da26e7fa00966a58afeb2114269b5512e59
Pierre-Clément Tosi <ptosi@google.com>
2022-07-14 09:38:48 +0100

    libfdt: Handle failed get_name() on BEGIN_NODE
    Validate the return value of fdt_get_name() as an ill-formed DT, causing
    it to fail, could result in fdt_check_full() dereferencing NULL.
    
    fixes: a2def5479950 ("libfdt: Check that the root-node name is empty")
    Signed-off-by: Pierre-Clément Tosi <ptosi@google.com>
    Message-Id: <20220714083848.958492-1-ptosi@google.com>
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>

--- libfdt/fdt_check.c
+++ libfdt/fdt_check.c
@@ -66,7 +66,7 @@ int fdt_check_full(const void *fdt, size_t bufsize)
 				int len;
 
 				name = fdt_get_name(fdt, offset, &len);
-				if (*name || len)
+				if (!name || *name || len)
 					return -FDT_ERR_BADSTRUCTURE;
 			}
 			break;
