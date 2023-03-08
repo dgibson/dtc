dgibson/dtc

commit c001fc01a43e7a06447c06ea3d50bd60641322b8

pylibfdt: fix swig build in install
A 'pip install' is silently broken unless the tree is dirty and contains
pylibfdt/libfdt.py. The problem is a known issue[1] with SWIG and
setuptools where the 'build_py' stage needing module.py runs before
the 'build_ext' stage which generates it. The work-around is to override
'build_py' to run 'build_ext' first.

[1] https://stackoverflow.com/questions/50239473/building-a-module-with-setuptools-and-swig

Signed-off-by: Rob Herring <robh@kernel.org>
Message-Id: <20220203180408.611645-2-robh@kernel.org>
Signed-off-by: David Gibson <david@gibson.dropbear.id.au>

setup.py
@@ -11,6 +11,8 @@ Written by Simon Glass <sjg@chromium.org>
 """
 
 from setuptools import setup, Extension
+from setuptools.command.build_py import build_py as _build_py
+
 import os
 import re
 import sys
@@ -40,11 +42,17 @@ libfdt_module = Extension(
     swig_opts=['-I' + os.path.join(srcdir, 'libfdt')],
 )
 
+class build_py(_build_py):
+    def run(self):
+        self.run_command("build_ext")
+        return super().run()
+
 setup(
     name='libfdt',
     use_scm_version={
         "root": srcdir,
     },
+    cmdclass = {'build_py' : build_py},
     setup_requires = ['setuptools_scm'],
     author='Simon Glass',
     author_email='sjg@chromium.org',

MANIFEST.in
@@ -6,7 +6,6 @@ include GPL
 include BSD-2-Clause
 include setup.py
 include pylibfdt/libfdt.i
-include pylibfdt/*.py
 include libfdt/libfdt.h
 include libfdt/fdt.h
 include libfdt/libfdt_env.h

commit 8759d3265f50ec290b7aca7feb5aa1d84e51b6df

pylibfdt: fix compatibility with python2
c001fc01a43e ("pylibfdt: fix swig build in install") introduced
python3-only super() invocation that fails with python2

  File "setup.py", line 48, in run
    return super().run()
TypeError: super() takes at least 1 argument (0 given)

while setup.py still claims to be compatible with both python versions:

>  While Python 3 is the default, it's also possible to invoke
>  this setup.py script with Python 2.

use legacy syntax that works in all python versions

Fixes: c001fc01a43e ("pylibfdt: fix swig build in install")
Signed-off-by: Jan Palus <jpalus@fastmail.com>

setup.py
@@ -45,7 +45,7 @@ libfdt_module = Extension(
 class build_py(_build_py):
     def run(self):
         self.run_command("build_ext")
-        return super().run()
+        return _build_py.run(self)
 
 setup(
     name='libfdt',