d4888958d64bd9b3e0e2e6582af665f9b81706c9
Johannes Beisswenger <johannes.beisswenger@cetitec.com>
2023-05-10 11:40:53 +0200

    tests: Add additional tests for device graph checks
    * bad-graph-child-address.dts: additional child address test since the
        one in bad-graph.dts is now shadowed by its prerequisites also
        failing.
    * bad-graph-reg-cells.dts: test warnings produced by check_graph_reg().
    
    Signed-off-by: Johannes Beisswenger <johannes.beisswenger@cetitec.com>
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -734,6 +734,9 @@ dtc_tests () {
     check_tests "$SRCDIR/bad-graph-root2.dts" graph_nodes
     check_tests "$SRCDIR/bad-graph-root3.dts" graph_nodes
     check_tests "$SRCDIR/bad-graph-root4.dts" graph_nodes
+    check_tests "$SRCDIR/bad-graph-reg-cells.dts" graph_endpoint
+    check_tests "$SRCDIR/bad-graph-reg-cells.dts" graph_port
+    check_tests "$SRCDIR/bad-graph-child-address.dts" graph_child_address
     run_sh_test "$SRCDIR/dtc-checkfails.sh" deprecated_gpio_property -- -Wdeprecated_gpio_property -I dts -O dtb "$SRCDIR/bad-gpio.dts"
     run_sh_test "$SRCDIR/dtc-checkfails.sh" -n deprecated_gpio_property -- -Wdeprecated_gpio_property -I dts -O dtb "$SRCDIR/good-gpio.dts"
     check_tests "$SRCDIR/bad-interrupt-cells.dts" interrupts_property
